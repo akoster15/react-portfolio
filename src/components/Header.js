b0cef0b44aa750cfa40ffbd7d03d9c4c8b1bdcf7
Andrew Koster <andrewkoster95@gmail.com>
2021-08-20 03:33:26 -0400

    added my about me page and worked more on header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Navbar = () => {
     return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
       <div className="container">
 
     <a className="navbar-brand" href="#">Andrew Koster</a>
@@ -13,7 +14,7 @@ const Navbar = () => {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ml-auto">
       <li className="nav-item active">
-        <a className="nav-link" href="#">About Me<span className="sr-only">(current)</span></a>
+        <a className="nav-link" href="#">About Me<span className="sr-only"></span></a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="#">Portfolio</a>
---
+++ src/components/pages/About.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import portrait from '../../images/portrait.jpg';
+
+const About = () => {
+    return (
+        <section class="section about-section gray-bg" id="about">
+    <div class="container">
+        <div class="row align-items-center justify-content-around flex-row-reverse">
+            <div class="col-lg-6">
+                <div class="about-text">
+                    <h3 class="dark-color">Do some awsome stuff with me.</h3>
+                    <h4 class="theme-color">UI / UX Designer &amp; Web Developer</h4>
+                    <p>I am orginally graduated from Clemson University with a civil engineering degree, but now have decided that I want to become a web developer. I am currently finishing </p>
+                    <p>I design and develop services for customers of all sizes, specializing in creating stylish, modern websites, web services and online stores.</p>
+                    <div class="btn-bar">
+                        <a class="px-btn theme" href="#">View Works</a>
+                        <a class="px-btn theme-t" href="#">Download CV</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5 text-center">
+                <div class="about-img">
+                    <img src={portrait}>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+    )
+};
+
+export default About;
