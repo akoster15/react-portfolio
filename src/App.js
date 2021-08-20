ca1ea8f1bd1789ae9c2f8df9f88cf998cbd04e0f
Andrew Koster <andrewkoster95@gmail.com>
2021-08-19 22:30:41 -0400

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>React Portfolio</h1>
     </div>
   );
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navigation = () => {
+    return (
+       <>
+        <Nav>
+            <NavigationContainer>
+                <NavLogo>
+                    dolla
+                </NavLogo>
+            </NavigationContainer>
+        </Nav>
+
+       </>
+    );
+};
+
+export default Navigation;
