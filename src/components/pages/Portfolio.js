629712289470ab166e5aa42ab78b6eeb8a27f871
Andrew Koster <andrewkoster95@gmail.com>
2021-08-20 16:45:10 -0400

    updated portfolio page and resume page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Project from '../Project';
 import PasswordGenerator from '../../images/password.PNG';
+import Planner from '../../images/planner.PNG';
+import Weather from '../../images/weather.PNG';
 
 export default function Portfolio() {
     return(
@@ -8,8 +10,8 @@ export default function Portfolio() {
             <h1 className="text-center">My Projects</h1>
             <div style={{display: "flex", flexWrap: "wrap", flexDirection: "row"}}>
             <Project name={"Password Generator"} img={PasswordGenerator} github={"https://github.com/akoster15/password-generator.git"} liveapp={"https://akoster15.github.io/password-generator/"} description={"This app displays will create a random password..."}/>
-            <Project name={"Password Generator"} img={PasswordGenerator} github={"https://github.com/akoster15/password-generator.git"} liveapp={"https://akoster15.github.io/password-generator/"} description={"This app displays will create a random password..."}/>
-            <Project name={"Password Generator"} img={PasswordGenerator} github={"https://github.com/akoster15/password-generator.git"} liveapp={"https://akoster15.github.io/password-generator/"} description={"This app displays will create a random password..."}/>
+            <Project name={"Work Day Scheduler"} img={Planner} github={"https://github.com/akoster15/work-day-scheduler.git"} liveapp={"https://akoster15.github.io/work-day-scheduler/"} description={"This app lets a user creat a schedule for work..."}/>
+            <Project name={"Weather Dashboard"} img={Weather} github={"https://github.com/akoster15/password-generator.git"} liveapp={"https://akoster15.github.io/password-generator/"} description={"This app displays the weather for a specific city..."}/>
 
             <Project name={"Password Generator"} img={PasswordGenerator} github={"https://github.com/akoster15/password-generator.git"} liveapp={"https://akoster15.github.io/password-generator/"} description={"This app displays will create a random password..."}/>
             <Project name={"Password Generator"} img={PasswordGenerator} github={"https://github.com/akoster15/password-generator.git"} liveapp={"https://akoster15.github.io/password-generator/"} description={"This app displays will create a random password..."}/>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Res from '../../images/resume.PNG'
 
 export default function Resume() {
     return(
         <div>
-            
+            <div className="row justify-content-md-center">
+                <div className="row xs12 sm={4} md4">
+                    <h1 className="text-center">Resume</h1>
+                        <img className="img-responsive img-fluid" src={Res} alt="resumeIMG" width="500" height="800"
+                    >
+                    </img>
+        </div>
+        </div>
         </div>
 
     );
