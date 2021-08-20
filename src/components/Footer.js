509d7c106770f7c02fa5e3361aa6673a02d18adb
Andrew Koster <andrewkoster95@gmail.com>
2021-08-20 14:15:20 -0400

    updated footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,32 @@ import React from 'react'
 
 export default function Footer() {
     return(
-        <div>
-            
-        </div>
+        <div className="main-footer font-small blue pt-4">
+            <div className="container-fluid text-center text-md-left">
+                <div className="row">
 
+                    <div className="col-md-3 mx-auto">
+                        <ul className="list-unstyled">
+                        <li><a href="https://github.com/akoster15"target="blank"><i class="fa fa-github"></i>GitHub</a></li>
+                        </ul>
+                    </div>
+
+                    <div className="col-md-3 mx-auto">
+                        <ul className="list-unstyled">
+                        <li><a href="https://www.linkedin.com/in/andrew-n-koster/"target="blank"><i class="fa fa-linkedin"></i>LinkedIn</a></li>
+                        </ul>
+                    </div>
+
+                    <div className="col-md-3 mx-auto">
+                        <ul className="list-unstyled">
+                        <li><a href="https://twitter.com/AKoster15"target="blank"><i class="fab fa-twitter"></i>Twitter</a>
+                        </li>
+                        </ul>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
     );
 }
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -10,8 +10,8 @@ export default function PortfolioContainer() {
     const [currentPage, setCurrentPage] = useState('Home');
   
     const renderPage = () => {
-      if (currentPage === 'About') {
-        return <About />;
+      if (currentPage === 'Resume') {
+        return <Resume />;
       }
       if (currentPage === 'Portfolio') {
         return <Portfolio />;
@@ -19,7 +19,7 @@ export default function PortfolioContainer() {
       if (currentPage === 'Contact') {
         return <Contact />;
       }
-      return <Resume />;
+      return <About />;
     };
   
     const handlePageChange = (page) => setCurrentPage(page);
