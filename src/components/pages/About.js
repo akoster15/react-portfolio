0d93d8c99f1501459e7b332610a8c5a9a5d1ad60
Andrew Koster <andrewkoster95@gmail.com>
2021-08-20 13:01:54 -0400

    updated About Me page. Got pictures and text to show on it

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,26 +3,29 @@ import portrait from '../../images/portrait.jpg';
 
 export default function About() {
     return (
-        <section class="section about-section gray-bg" id="about">
-    <div class="container">
-        <div class="row align-items-center justify-content-around flex-row-reverse">
-            <div class="col-lg-6">
-                <div class="about-text">
-                    <h3 class="dark-color">Do some awsome stuff with me.</h3>
-                    <h4 class="theme-color">UI / UX Designer &amp; Web Developer</h4>
-                    <p>I am orginally graduated from Clemson University with a civil engineering degree, but now have decided that I want to become a web developer. I am currently finishing </p>
-                    <p>I design and develop services for customers of all sizes, specializing in creating stylish, modern websites, web services and online stores.</p>
-                
+    <div className="container">
+        <div className="section">
+
+            <div className="row">
+                <div className="col s12 m4">
+                    <img className="img-responsive img-fluid" src={portrait} alt="pofileImg" width="450" heigh="600"
+                    >
+                    </img>
                 </div>
-            </div>
-            <div class="col-lg-5 text-center">
-                <div class="about-img">
-                    <img src={portrait}>
+
+                <div className="col s12 m4">
+                    <h3><i className="mdi-content-send"></i></h3>
+                    <h4>About Me</h4>
+                    <p className="left-align light">
+                        My name is Andrew Koster. I was born in Johns Creek, GA and have lived hear my whole life.
+                        I have recently decided to make a career change from being a civil engineer to become a 
+                        wed developer. I am currently studying HTML, CSS, Javascript, Bootstrap, JQuery, NodeJS, Git,
+                        React, and so much more. I love to code and create new ideas. I also love going to the gym, 
+                        watching football, playing basketball, traveling, and going out with friends.
+                    </p>
                 </div>
             </div>
         </div>
     </div>
-    </div>
-</section>
     );
 }
